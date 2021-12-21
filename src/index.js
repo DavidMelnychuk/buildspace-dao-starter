ac649aa27f308795e65d7022cf1c1e8874e223c7
David <DavidMelnychuk@users.noreply.github.com>
2021-12-20 23:20:59 -0800

    add connect wallet button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+const RINKEBY_CHAIN_ID = 4;
+const supportedChainIds = [RINKEBY_CHAIN_ID];
+
+const connectors = {
+  injected: {}
+};
 
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <div className="landing">
+        <App />
+      </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
